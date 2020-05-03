d25f55128395cebbeff78a5634aec78b8d6775d2
AlexGuru <zong.an@yandex.ru>
2020-05-03 18:30:32 +0300

    remove app module

--- app/src/main/java/com/ag/navigationcomponentsample/activities/MultipleToolbarComboActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.ag.navigationcomponentsample.activities
-
-import androidx.appcompat.app.AppCompatActivity
-import com.ag.navigationcomponentsample.R
-
-class MultipleToolbarComboActivity : AppCompatActivity(R.layout.activity_combo)
